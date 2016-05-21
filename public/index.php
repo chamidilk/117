f83c04c486f4d5d6411df2a03688dfc18a22e2dd
Ramindu Deshapriya <rasade88@gmail.com>
2016-05-21 22:07:01 +0530

    Added MySQL connection

--- public/index.php
+++ public/index.php
@@ -8,7 +8,21 @@ $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
+    $db = getConnection();
 
     return $response;
 });
 $app->run();
+
+function getConnection() {
+    try {
+        $db_username = "rdeshapr_ramindu";
+        $db_password = "181mihiri";
+        $conn = new PDO('mysql:host=67.20.48.4;dbname=rdeshapr_117', $db_username, $db_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+    return $conn;
+}
