c85ad805bb346bc7dabfbedb24f4c3cd6bc118b4
Chandima Jayawickrema <cjayawickrema@gmail.com>
2016-05-21 23:15:20 +0530

    adding UI code

---
+++ ui/scripts/controllers/RequestHelpController.js
@@ -0,0 +1,30 @@
+/* 
+ * 
+ */
+
+
+function RequestHelpController($scope, $state) {
+
+    $scope.languages = ['SINHALA', 'TAMIL', 'ENGLISH'];
+
+    $scope.textMap = {
+        SINHALA: {
+            language: 'සින්හල',
+            request_help: 'උපකාර ඉල්ලුම්'
+        },
+        ENGLISH: {
+            language: 'English',
+            request_help: 'Request Help'
+        },
+        TAMIL: {
+            language: 'தமிழ்',
+            request_help: 'உதவி வேண்டுகோள்'
+        }
+    };
+
+    $scope.setLanguage = function (language) {
+        $scope.language = language;
+        $state.go();
+    };
+
+}
