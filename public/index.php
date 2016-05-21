f0d8795016dc7e1dc90e9975c10d4fb2d1ed558e
Ramindu Deshapriya <rasade88@gmail.com>
2016-05-22 00:33:37 +0530

    Added request post

--- public/index.php
+++ public/index.php
@@ -7,11 +7,72 @@ require '../vendor/autoload.php';
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+
     $db = getConnection();
 
-    return $response;
+    return $response->withJson(array('name' => $name));
 });
+$app->post('/request', function (Request $request, Response $response) {
+    $body = $request->getParsedBody();
+    $db = getConnection();
+    $person = new stdClass();
+    $person->per_ID = null;
+    $person->nationalID = $body['nationalID'];
+    $person->per_fullname = $body['per_fullname'];
+    $person->per_mobile = $body['per_mobile'];
+    $person->per_phone_other = $body['per_phone_other'];
+    $person->per_organization = $body['per_organization'];
+    $person->per_email = $body['per_email'];
+    $person->per_comments = "";
+    $person->per_status_REF = 0;
+
+    $personSql = "SELECT * FROM Person WHERE nationalID=:nationalID";
+    $personStmt = $db->prepare($personSql);
+    $personStmt->bindParam("nationalID", $person->nationalID );
+    $personStmt->execute();
+    $storedPerson = $personStmt->fetchAll(PDO::FETCH_ASSOC);
+    if(!isset($storedPerson['per_ID'])) {
+        // person does not exist, store as a new person
+        try {
+            $person->per_ID = insertObject($db,'Person', $person);
+        } catch(PDOException $pdoe) {
+            return $response->withJson(array('error' => 'New person could not be entered',
+                'detail' => $pdoe->getMessage()), 500);
+        }
+
+    } else {
+        $person->per_ID = $storedPerson['per_ID'];
+    }
+
+
+    $resourceRequest = new stdClass();
+    $resourceRequest->req_ID = null;
+    $resourceRequest->req_made_date = $body['req_made_date'];
+    $resourceRequest->req_close_date = $body['req_close_date'];
+    $resourceRequest->req_type_REF = $body['req_type_REF'];
+    $resourceRequest->requestor_per_ID = $person->per_ID;
+    $resourceRequest->donor_per_ID = 0;
+    $resourceRequest->reqarea_ID = $body['reqarea_ID'];
+    $resourceRequest->req_address = $body['req_address'];
+    $resourceRequest->req_GPS = $body['req_GPS'];
+    $resourceRequest->req_for_people = $body['req_for_people'];
+    $resourceRequest->req_for_adults = $body['req_for_adults'];
+    $resourceRequest->req_for_kids = $body['req_for_kids'];
+    $resourceRequest->req_for_infants = $body['req_for_infants'];
+    $resourceRequest->req_summary = $body['req_summary'];
+    $resourceRequest->req_details = $body['req_details'];
+    $resourceRequest->reqstatus_REF = 0;
+
+    try {
+        $resourceRequest->req_ID = insertObject($db,'Request', $resourceRequest);
+    } catch(PDOException $pdoe) {
+        return $response->withJson(array('error' => 'New request could not be added',
+            'detail' => $pdoe->getMessage()), 500);
+    }
+
+    return $response->withJson($resourceRequest, 200);
+});
+
 $app->run();
 
 function getConnection() {
@@ -26,4 +87,21 @@ function getConnection() {
         echo 'ERROR: ' . $e->getMessage();
     }
     return $conn;
+}
+
+function insertObject($db, $table, &$object, $keyname = NULL) {
+    foreach(get_object_vars($object) as $field => $value) {
+        $ins[] = ":" . $field;
+    }
+    $ins = implode(',', $ins);
+    $fields = implode(',', array_keys(get_object_vars($object)));
+    $sql = "INSERT INTO $table ($fields) VALUES ($ins)";
+
+    $sth = $db->prepare($sql);
+    foreach (get_object_vars($object) as $f => $v)
+    {
+        $sth->bindValue(':' . $f, $v);
+    }
+    $sth->execute();
+    return $db->lastInsertId();
 }
