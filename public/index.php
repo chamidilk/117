0a3decef23f17b724ce98a540e467fcb4b1fda49
Ramindu Deshapriya <rasade88@gmail.com>
2016-05-21 22:39:35 +0530

    Added local changes

--- public/index.php
+++ public/index.php
@@ -17,9 +17,9 @@ $app->run();
 function getConnection() {
     // mysql://b8b5ceedc559fd:d58b20ed@us-cdbr-iron-east-04.cleardb.net/heroku_2ceffc3dc0a99d5?reconnect=true
     try {
-        $db_username = "b8b5ceedc559fd";
-        $db_password = "d58b20ed";
-        $conn = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_2ceffc3dc0a99d5', $db_username, $db_password);
+        $db_username = "root";
+        $db_password = "";
+        $conn = new PDO('mysql:host=localhost;dbname=117', $db_username, $db_password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch(PDOException $e) {
