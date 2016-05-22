0b703128eb19ac961cc643f7e29b0bec9a7257bb
Chamindra de Silva <chamindra@gmail.com>
2016-05-22 11:29:23 +0530

    added propose help messages

--- ui/scripts/controllers/RequestHelpController.js
+++ ui/scripts/controllers/RequestHelpController.js
@@ -11,14 +11,17 @@ function RequestHelpController($scope, $state) {
         SINHALA: {
             language: 'සිංහල',
             request_help: 'උපකාර ඉල්ලුම්'
+            propose_help: 'උපකාර ඉල්ලුම්'
         },
         ENGLISH: {
             language: 'English',
             request_help: 'Request Help'
+            propose_help: 'Give Help'
         },
         TAMIL: {
             language: 'தமிழ்',
             request_help: 'உதவி வேண்டுகோள்'
+            propose_help: 'உதவி வேண்டுகோள்'
         }
     };
 
