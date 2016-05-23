c88dc7bf61a3aae1aa8d3bf7dab22664a40246cc
Pubudu Welagedara <pwelagedara@virtusa.com>
2016-05-23 20:26:17 +0530

    changed base url

--- ui/scripts/controllers/ModalController.js
+++ ui/scripts/controllers/ModalController.js
@@ -26,7 +26,7 @@ function ModalController($scope, $http, $uibModalInstance, currentStatus, req_ID
     console.log(request);
     $http({
             method: 'POST',
-            url: 'http://220.247.222.29/one-one-seven/public/requests/status',
+            url: 'http://117.dmc.gov.lk/one-one-seven/public/requests/status',
             data: request
         }).then(function successCallback(response) {
             console.log(response);
