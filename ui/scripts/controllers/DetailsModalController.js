808875b30bcd16db1a9465c3583ac4e0de226eeb
Pubudu Welagedara <pwelagedara@virtusa.com>
2016-05-25 19:06:00 +0530

    added the service calls to retireve comments

--- ui/scripts/controllers/DetailsModalController.js
+++ ui/scripts/controllers/DetailsModalController.js
@@ -5,7 +5,34 @@
 
 function DetailsModalController($scope, $http, $uibModalInstance, $cookies, requestSelected) {
     
-    $scope.requestSelected = requestSelected;
+  $scope.requestSelected = requestSelected;
+  $scope.comments = [];
+  
+
+  $scope.loadComments = function(){
+
+    var encodedCredentials = $cookies.get('encodedCredentials');
+
+    if(encodedCredentials == "" || encodedCredentials == undefined || encodedCredentials == null){
+      $state.go('login');
+    }
+
+    $http({
+            method: 'GET',
+            url: 'http://117.dmc.gov.lk/one-one-seven/public/requests/' + $scope.requestSelected.req_ID + '/status',
+            headers: {
+              'X-Authorization': 'Basic '+ encodedCredentials
+            },
+            params: $scope.filters
+    }).then(function successCallback(response) {
+            $scope.comments = response.data;
+            console.log(response);
+    }, function errorCallback(response) {
+            console.error(response);
+    });
+  }
+
+  $scope.loadComments();
 
   $scope.ok = function () {
       
